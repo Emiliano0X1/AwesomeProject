142dda886022bb362cc87e68d53619a45823e3dc
Emiliano0X1 <egp2506@gmail.com>
2024-06-10 22:10:25 -0700

    En esta actualizacion, implemente la pportada de la aplicacion e intente poner las demas screens pero hay algunos errores que solucionare mañana ( Day One)

---
+++ Screens/home.js
@@ -0,0 +1,11 @@
+
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+import React from 'react';
+import {StyleSheet, Text, View,Button,Alert, TouchableOpacity, ImageBackground,Image} from 'react-native';
+
+
