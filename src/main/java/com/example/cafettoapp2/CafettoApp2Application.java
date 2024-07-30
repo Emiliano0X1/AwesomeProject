07612dfb26d817efc58b6643f8c2a88495e958a8
Emiliano0X1 <egp2506@gmail.com>
2024-07-30 13:24:58 -0700

    Initial commit for Spring Boot project

---
+++ src/main/java/com/example/cafettoapp2/CafettoApp2Application.java
@@ -0,0 +1,13 @@
+package com.example.cafettoapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafettoApp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafettoApp2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cafettoapp2/CafettoApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cafettoapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafettoApp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
