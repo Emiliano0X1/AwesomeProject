94e7571e72f7e6e797ae436b155e33b050bc628b
Emiliano0X1 <egp2506@gmail.com>
2024-07-30 14:16:38 -0700

    This is the first commit i made the classes for the product, also i need more

--- src/main/java/com/example/cafettoapp2/Producto/TypeProducts/productoSmoothie.java
+++ src/main/java/com/example/cafettoapp2/Producto/TypeProducts/productoSmoothie.java
@@ -1,4 +1,19 @@
 package com.example.cafettoapp2.Producto.TypeProducts;
 
 public class productoSmoothie extends Producto{
+
+    private String description;
+
+    public productoSmoothie(String name, int price, String type, String description) {
+        super(name, price, type);
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
